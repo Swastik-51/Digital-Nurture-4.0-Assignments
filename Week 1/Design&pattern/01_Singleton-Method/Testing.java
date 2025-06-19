bf324208d140687480525e5e2f4b03fc05bec98a
Swastik Sengupta <swastik724@gmail.com>
2025-06-19 23:28:39 +0530

    Add files via upload

---
+++ Week 1/Design&pattern/01_Singleton-Method/Testing.java
@@ -0,0 +1,20 @@
+public class Testing {
+    public static void main(String[] args) {
+        // Get the single instance of Logger
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+        
+        System.out.println();
+
+        // Check if both references point to the same instance
+        if (logger1 == logger2) {
+            System.out.println("Logger instances are the same.");
+        } else {
+            System.out.println("Logger instances are different.");
+        }
+        
+        // Test logging
+        logger1.log("This is a test 1 log message.");
+        logger2.log("This is a test 2 log message.");
+    }
+}
